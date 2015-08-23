55bb4ff748c77dde74f02b15651b5df9e538c2da
sederaa <seb@Sebs-MacBook-Pro.local>
2015-08-23 19:02:13 +1200

    Fix deployment script 15

--- deploy.sh
+++ deploy.sh
@@ -106,7 +106,7 @@ selectNodeVersion
 # 3. Install npm packages
 if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
   cd "$DEPLOYMENT_SOURCE"
-  eval $NPM_CMD install --production
+  eval $NPM_CMD install #--production
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
